58e9f58e6fc3362e64321beb2db29e3ddcbede72
Lucas <123456789@qq.com>
2020-12-10 10:42:27 +0800

    mixin 扩展类

--- users/views.py
+++ users/views.py
@@ -2,12 +2,33 @@ from django.shortcuts import render, redirect
 from django.views import View
 from users.models import Users
 from django.http import HttpResponse, JsonResponse
+from django.utils.decorators import method_decorator
 
+# def is_login(request):
+#     username = request.session.get('username')
+#     if username:
+#         return HttpResponse(f'<h1>{username}用户已经登录, 这就是登录页面<h1/>')
 
+def is_login(view_func):
+    def wepper(request):
+        username = request.session.get('username')
+        if username:
+            return HttpResponse(f'<h1>{username:}已登录,这个是登录后的页面<h1/>')
+        return view_func(request)
+    return wepper
+
+class Is_login(object):
+    @classmethod
+    def as_view(cls, **initkwargs):
+        view_func = super().as_view(**initkwargs)
+        return is_login(view_func)
 
 class Register(View):
+
+
     def get(self, request):
         return render(request, 'register.html')
+
 
     def post(self, request):
         username = request.POST.get('username')
@@ -16,15 +37,14 @@ class Register(View):
         return redirect('/login/')
 
 
-class Login(View):
+class Login(Is_login, View):
+
+
     def get(self, request):
         # username = request.COOKIES.get('username')
         # return render(request, 'login.html', {'username': username})
-        id = request.session.get('id')
-        if id:
-            return JsonResponse({'message': 'already login'})
-        else:
-            return render(request, 'login.html')
+
+        return render(request, 'login.html')
 
 
     def post(self, request):
@@ -39,7 +59,7 @@ class Login(View):
 
             response = JsonResponse({'message': 'login sucuss'})
             request.session['id'] = user.id
-            request.session['password'] = user.passworld
+            request.session['username'] = user.username
             if remember != 'true':
                 request.session.set_expiry(0)
             return response
